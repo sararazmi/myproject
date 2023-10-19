3def02df7754b5ff37a555f293de93d2ff3e4617
sara <sara.razmi.89@gmail.com>
2023-10-19 05:00:15 +0330

    revert the changes

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 def home(request):
@@ -7,9 +7,11 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 
-
 def board_topics(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
 
