6b25f4542c22400ba35c0ec8d4b84a2cba19caae
Sara Razmi <sara.razmi.89@gmail.com>
2024-01-13 05:04:10 +0330

    created accouut

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
+
+    'accounts',
     'boards',
 ]
 
@@ -79,8 +82,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "test",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
+
     }
 }
 
