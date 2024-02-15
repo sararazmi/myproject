c1b73aaf5a436edda6b261198caa9f76b5761058
Sara Razmi <sara.razmi.89@gmail.com>
2024-02-15 06:23:24 +0330

    add .env

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -150,4 +151,4 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
+SECRET_KEY = config('SECRET_KEY')
